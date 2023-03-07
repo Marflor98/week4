01e3442a7764eb3a1bb34a151f811d1e4b1178db
Marflor98 <100226529+Marflor98@users.noreply.github.com>
2023-03-07 08:53:09 -0600

    Update letterCounter.kt

--- letterCounter.kt
+++ letterCounter.kt
@@ -1,1 +1,13 @@
-
+var userstr : string // The user's string
+var userchar : char //The user's character
+var charinstr : int // Number of times character is in the user's string
+//Get user's string
+print("Enter a string.")
+userstr = readLine()!!
+  //Get user's character
+  print("Enter a character.")
+  userchar = readLine()!!
+  //Get number of times the user's character is in the user's string
+  for (userchar in userstr) {
+    println(Number)
+  }
