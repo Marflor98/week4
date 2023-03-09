babb879dec94e615c036c915b333a5e6fced813c
Marflor98 <100226529+Marflor98@users.noreply.github.com>
2023-03-09 10:49:18 -0600

    Update barChart.kt

--- barChart.kt
+++ barChart.kt
@@ -1,5 +1,20 @@
-val store1
-val store2
-val store3
-val store4
-val store5
+val store1 : Int
+val store2 : Int
+val store3 : Int
+val store4 : Int
+val store5 : Int 
+//Get store1 sales
+print("Enter today's sales for store 1:")
+store1 = readLine()!!to.Int()
+//Get store2 sales
+print("Enter today's sales for store 2:")
+store2 = readLine()!!to.Int()
+//Get store3 sales
+print("Enter today's sales for store 3:")
+store3 = readLine()!!to.Int()
+//Get store4 sales
+print("Enter today's sales for store 4:")
+store4 = readLine()!!to.Int()
+//Get store5 sales
+print("Enter today's sales for store 5:")
+store5 = readLine()!!to.Int()
